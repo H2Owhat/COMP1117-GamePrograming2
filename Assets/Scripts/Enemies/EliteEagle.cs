32b5f2c4e446accb29113648f32f0841130c66cc
H2Owhat <talon2523@gmail.com>
2026-02-03 23:22:45 -0500

    week4sideQuestA

--- Assets/Scripts/Enemies/EliteEagle.cs
+++ Assets/Scripts/Enemies/EliteEagle.cs
@@ -1,39 +1,48 @@
 using UnityEngine;
 
-public class Enemy : Character
+public class EliteEagle : Enemy
 {
-    [Header("Enemy Settings")]
-    [SerializeField] protected float patrolDistance = 5.0f;
+    private SpriteRenderer sRender;
+    
 
-    protected Vector2 startPos;
-    protected int direction = -1;
     protected override void Awake()
     {
-       base.Awake();
-        startPos = transform.position;
+        sRender = GetComponent<SpriteRenderer>();
+            base.Awake();
+        ChangeColour();
+       
     }
-
+   
     private void Update()
     {
+       
+
         //calculate boundries of my movement
         float leftBoundary = startPos.x - patrolDistance;//staring position - patrol distance
         float rightBoundary = startPos.x + patrolDistance;
 
         //move enemy
-        transform.Translate(Vector2.right * direction * MoveSpeed * Time.deltaTime);
+        transform.Translate(Vector2.right * direction * MoveSpeed*(2) * Time.deltaTime);
 
         //flip enemy when hits boundary
         if (transform.position.x >= rightBoundary)
         {
             direction = -1; // go to the left
-            transform.localScale = new Vector3(1, 1, 1);
+            transform.localScale = new Vector3(2, 2, 2);
         }
         else if (transform.position.x <= leftBoundary)
         {
             direction = 1;
-            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(-2, 2, 2);
         }
     }
 
-    
+    void ChangeColour()
+    {
+        sRender.color = new Color(1, 1, 0, 1);
+    }
+
+   
+
+
 }
